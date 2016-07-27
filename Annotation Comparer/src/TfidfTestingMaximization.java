f3c9d90798d6b08b186490e818c5921ea09d9a53
FishmanL <leor.fishman@gmail.com>
2016-07-27 12:14:03 -0400

    Update TfidfTestingMaximization.java

--- Annotation Comparer/src/TfidfTestingMaximization.java
+++ Annotation Comparer/src/TfidfTestingMaximization.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 import java.util.Arrays;
-/**
- *
- * @author leorf_000
- */
+
  //this whole class is just functions useful for wordcomparer, the main is just for testing the functions
 public class TfidfTestingMaximization extends TfIdfAnalysis {
 	//averaging an array of doubles
